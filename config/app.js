f940faff18ca11f207de3e63216bc7823acfcaec
veyselboybay <veysel.boybay@outlook.com>
2020-10-25 15:46:26 -0400

    mongoDB connection is done

--- config/app.js
+++ config/app.js
@@ -4,21 +4,36 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+// MongoDB connection configuration
+let mongoose= require('mongoose');
+let DB = require('./db');
+mongoose.connect(DB.URI,{useNewUrlParser:true,useUnifiedTopology:true});
+
+let mongoDB = mongoose.connection;
+mongoDB.on('error',console.error.bind(
+  console,'Connection Error:'
+));
+mongoDB.once('open',() =>
+{
+  console.log('Connected to MongoDB...');
+});
+
+
+let indexRouter = require('../routes/index');
+let usersRouter = require('../routes/users');
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
