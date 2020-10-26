0903f6294ecba41fb4d27e7e6d5a24d43391acb6
veyselboybay <veysel.boybay@outlook.com>
2020-10-26 05:52:29 -0400

    heroku uri setup

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI":"mongodb://localhost/contacts"
+    //"URI":"mongodb://localhost/contacts"
+    "URI":"mongodb+srv://veysel:7LUoX2GiinoKXPLA@cluster0.xibqz.mongodb.net/contacts?retryWrites=true&w=majority"
 }
